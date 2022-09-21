94562cd7394219f35da2edddb59e6ee77e5de8dc
Anunay Rao <anunay@Anunays-Device.local>
2022-09-20 17:47:25 -0700

    Review Comments

--- Krit/palindrome.py
+++ Krit/palindrome.py
@@ -7,22 +7,25 @@ class Node:
     def __init__(self,item):
         self.item = item
         self.next = None
-    
-# 1 -> 2 -> 3 -> 2 -> 1
 
 def checkPalindrome(head): # 1-> 2 -> 3  
     curr = head #1 
-    reversedList_head = Node(curr.item) #1
+    #reversedList_head = Node(curr.item) #1
+    reverseCopy = Node(head.item)
     curr = curr.next #2
-    curr1 = reversedList_head.next #None
+    #curr1 = reversedList_head.next #None
+    curr1 = reverseCopy
+    
     while curr: 
-        curr1 = Node(curr.item) #2 3
+        #curr1 = Node(curr.item) #2 3
+        curr1.next = Node(curr.item)
         curr1 = curr1.next #None None
         curr = curr.next #3 None
     
     
-    
-    prev = reversedList_head #1
+    printList(reverseCopy)
+    #prev = reversedList_head #1
+    prev = reverseCopy
     #curr = prev
     curr = prev.next #2
     while curr:
@@ -31,10 +34,22 @@ def checkPalindrome(head): # 1-> 2 -> 3
         prev = curr #2 3
         curr = temp #3 None
     curr = head
-    curr1 = reversedList_head
+    curr1 = prev
+    #curr1 = reversedList_head -> Not reversed copy
     while curr:
-        if curr != curr1:
+        if curr.item != curr1.item: #logical error
             return False
         curr = curr.next
-        curr1 = curr.next
+        curr1 = curr1.next #logical error
     return True 
+
+def printList(node):
+    while node:
+        print(node.item)
+        node = node.next
+
+if __name__ == "__main__":
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    print(checkPalindrome(head))
